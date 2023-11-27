4f2ac61d35e2060b23d6d5ac0cd01995570ab3fa
Ali Kooshesh <alikooshesh007@gmail.com>
2023-11-27 20:31:52 +0330

    edit and complete

--- src/App.js
+++ src/App.js
@@ -4,28 +4,71 @@ import Button from './components/button/Button';
 import Input from './components/input/input';
 import TodoBox from './components/todoBox/TodoBox';
 
+import { BsAirplaneFill } from "react-icons/bs";
+
 function App() {
 
   const [data, setData] = useState([])
 
   const [inpVal , setInpVal] = useState("")
 
+  const [formMode , setFormMode] = useState({mode : "add" , id : null})
+
   function removeTodo(todoId){
     setData(data.filter(item => item.id != todoId))
+  }
+
+  function completeTodo(todoId){
+    console.log(todoId)
+    setData(data.map(item =>{
+      if(item.id == todoId){
+        item.isComplete = true
+        return item
+      }else{
+        return item
+      }
+    }))
+  }
+
+  function editTodo(todoId, newText){
+    setData(data.map(item =>{
+      if(item.id == todoId){
+        item.text = newText
+        return item
+      }else{
+        return item
+      }
+    }))
+
+    setFormMode({mode : "add" , id : null})
+  }
+
+  function changeToEditMode(todoId){
+    setFormMode({mode : "edit" , id : todoId})
   }
 
   return (
     <div className='container'>
       <div className="addNewTodo-wrapper">
         <Input valueState={inpVal} onChangeHandler={(e)=> setInpVal(e.target.value)}/>
-        <Button onclickHandler={()=>setData([...data,{id : Date.now() , text : inpVal}])}>+</Button>
+        {formMode?.mode == 'edit' ?
+          <Button onclickHandler={()=>editTodo(formMode.id , inpVal)}>ok</Button> :
+          <Button onclickHandler={()=>setData([...data,{id : Date.now() , text : inpVal, isComplete : false}])}>+</Button>
+        }
       </div>
 
       <div className='todoList-wrapper'>
 
         {data.map(item => {
           return(
-            <TodoBox id={item.id} text={item.text} removeTodo={removeTodo}/>
+            <TodoBox 
+              id={item.id} 
+              text={item.text} 
+              removeTodo={removeTodo} 
+              isComplete={item.isComplete}
+              completeTodoFunction={completeTodo}
+              changeToEditMode={changeToEditMode}
+            />
           )
         })}
       </div>
--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,5 +1,5 @@
 function Input({valueState,onChangeHandler}){
-    return <input value={valueState} onChange={onChangeHandler}/>
+    return <input value={valueState} onChange={onChangeHandler} className="border"/>
 }
 
 export default Input
