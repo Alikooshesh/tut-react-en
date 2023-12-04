13357a45315fcf63adb4b701656f51f180e4c2ee
Ali Kooshesh <alikooshesh007@gmail.com>
2023-12-04 21:10:47 +0330

    final-commit

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,8 +1,8 @@
 import './button.css'
 
-function Button({children,onclickHandler}){
+function Button({children,onclickHandler,style,disabled}){
     return(
-        <button className='btn' onClick={onclickHandler}>
+        <button className='btn' onClick={onclickHandler} style={style} disabled={disabled}>
             {children}
         </button>
     )
