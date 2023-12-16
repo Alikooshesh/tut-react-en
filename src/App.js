3d173ad61cfa75c2ea8c2d817bb960a618c6cfc6
Ali Kooshesh <alikooshesh007@gmail.com>
2023-12-16 18:55:33 +0330

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
-import logo from './logo.svg';
-import TestText from './components/testText/TestText'
-import Button from './components/button/Button';
-import './App.css';
-import './newStyle.css'
+import { useEffect } from "react";
 
 function App() {
 
-  const test = 5;
-  console.log(logo)
+  
+
+  function btnClickHandler(){
+    let user = {
+      name : "ali",
+      age : 23
+    }
+    localStorage.setItem('firstTest' , JSON.stringify(user))
+  }
+
+  useEffect(()=>{
+    let ls = localStorage.getItem('firstTest')
+    ls = JSON.parse(ls)
+    console.log(ls)
+  },[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestText/>
-        <TestText/>
-        <TestText/>
-        <Button/>
-        <TestText/>
-        <TestText/>
-        <TestText/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className='my-text'>its my first changes {test + 7}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=> btnClickHandler()}>click me</button>
     </div>
   );
 }
--- src/components/button/Button.js
+++
@@ -1,9 +0,0 @@
-import './button.css'
-
-function Button(){
-    return(
-        <button className='btn'>click here!</button>
-    )
-}
-
-export default Button
